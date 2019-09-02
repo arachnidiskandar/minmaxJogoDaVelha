b336637b343796806fd10a074ac0f4b53488c4d0
Augusto <augstarachrafael@gmail.com>
2019-09-02 11:17:31 -0300

    pseudocódigo minmax

--- minmax.py
+++ minmax.py
@@ -1,2 +1,22 @@
 import sys
-print(sys.argv)
+#recebe o argumento x_o_x_o__
+#tabuleiro = sys.argv[1]
+'''
+minimax(estado, profundidade)
+ SE estado é terminal OU profundidade = 0 ENTÃO
+ RETORNE o valor da heurística do estado
+ SENÃO SE o estado representa a jogada de algum adversário ENTÃO
+ valor ← +∞
+ PARA CADA filho DE estado
+ valor ← min(valor, minimax(filho, profundidade-1))
+ FIM PARA
+ RETORNE valor
+ SENÃO
+ valor ← -∞
+ PARA CADA filho DE estado
+ valor ← max(valor, minimax(filho, profundidade-1))
+ FIM PARA
+ RETORNE valor
+ FIM SE
+FIM
+'''
