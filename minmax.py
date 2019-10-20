953c807937ec36cb6659655db55bdf923195a842
Augusto <augstarachrafael@gmail.com>
2019-10-20 03:51:43 -0300

    retornando jogada

--- minmax.py
+++ minmax.py
@@ -3,30 +3,52 @@ import sys
 #tabuleiro = sys.argv[1]
 import textwrap
 
-def minimax(estado, depth):
+#def minimax(estado, depth):
 
 
-estado = "ooxo_oxoo"
+estado = "xo_xo__o_"
 
-#Validar estado terminal
-def validarEstado(estado):
-    if '_' in estado:
-        tabuleiro = textwrap.wrap(estado, 3)
+
+def checarVitoria(estado):
+    if '_' in estado: #verifica se n está cheio
+        tabuleiro = textwrap.wrap(estado, 3)    #separa a string em 3 vetores
         for linha in range(3):
             if (tabuleiro[linha][0] == tabuleiro[linha][1] and \
-                tabuleiro[linha][2] == tabuleiro[linha][2]):
+                tabuleiro[linha][2] == tabuleiro[linha][2]):#checa vitória em linhas
                 return 1
         if tabuleiro[0][0] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][2] or \
-            tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][0]:
+            tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][0]:#checa vitória nas diagonais
             return 1
-    else:
+    else:#empate
         return 0
 
-def achaJogadasPossíveis(tabuleiro):
+def achaJogadasPossíveis(tabuleiro):#pegas as jogadas váliadas
     movimentos_validos = []
     for i, j in enumerate(tabuleiro):
         if j == '_':
-            movimentos_validos.append(j)
+            movimentos_validos.append(i)
     return movimentos_validos
+
+def checarVez(estado):
+    if estado is "_________":
+        return 'vezDoX'
+    elif estado.count('x') > estado.count('o'):
+        return 'vezDoO'
+    else:
+        return 'vezDoX'
+
+def compMov(estado):
+    jogadasPossiveis = achaJogadasPossíveis(estado)
+    jogada = 0
+    for letra in ['o', 'x']: #Checar se tem alguma jogada vencedora
+        for i in jogadasPossiveis:
+            copiaEstado = list(estado)
+            copiaEstado[i] = letra
+            novoEstado = ''.join(copiaEstado)
+            if checarVitoria(novoEstado):
+                jogada = i
+                return jogada
+
+print(compMov(estado))
 
 
