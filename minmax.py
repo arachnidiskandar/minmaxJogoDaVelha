57a080dfca269e46f7edccfdb1be89aab538d7b1
Augusto <augstarachrafael@gmail.com>
2019-10-20 02:18:31 -0300

    primeiras linhas do algoritmo

--- minmax.py
+++ minmax.py
@@ -1,22 +1,32 @@
 import sys
 #recebe o argumento x_o_x_o__
 #tabuleiro = sys.argv[1]
-'''
-minimax(estado, profundidade)
- SE estado é terminal OU profundidade = 0 ENTÃO
- RETORNE o valor da heurística do estado
- SENÃO SE o estado representa a jogada de algum adversário ENTÃO
- valor ← +∞
- PARA CADA filho DE estado
- valor ← min(valor, minimax(filho, profundidade-1))
- FIM PARA
- RETORNE valor
- SENÃO
- valor ← -∞
- PARA CADA filho DE estado
- valor ← max(valor, minimax(filho, profundidade-1))
- FIM PARA
- RETORNE valor
- FIM SE
-FIM
-'''
+import textwrap
+
+def minimax(estado, depth):
+
+
+estado = "ooxo_oxoo"
+
+#Validar estado terminal
+def validarEstado(estado):
+    if '_' in estado:
+        tabuleiro = textwrap.wrap(estado, 3)
+        for linha in range(3):
+            if (tabuleiro[linha][0] == tabuleiro[linha][1] and \
+                tabuleiro[linha][2] == tabuleiro[linha][2]):
+                return 1
+        if tabuleiro[0][0] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][2] or \
+            tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[1][1] == tabuleiro[2][0]:
+            return 1
+    else:
+        return 0
+
+def achaJogadasPossíveis(tabuleiro):
+    movimentos_validos = []
+    for i, j in enumerate(tabuleiro):
+        if j == '_':
+            movimentos_validos.append(j)
+    return movimentos_validos
+
+
